c02e5f59baf753df08c3274b4f7bc19ddbd59f6e
SamuelCs99 <samyappx@gmail.com>
2023-08-13 13:55:49 -0400

    variable de entornos y tabla en bd postgres vercel

--- Easser_app/models.py
+++ Easser_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class series(models.Model):
+    id_serie = models.AutoField(primary_key=True)
+    serie = models.CharField(max_length=75)
+    url = models.URLField()
