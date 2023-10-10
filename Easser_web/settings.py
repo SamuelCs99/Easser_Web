804ca9995b71676e990c9d43d08f849b5398112b
SamuelCs99 <samyappx@gmail.com>
2023-10-10 11:35:30 -0400

    solucion problema de archivos estaticos (edicion de vercel.json, build_files y ejecucion collectstatic)

--- Easser_web/settings.py
+++ Easser_web/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Easser_web/static')
 ]
 
-STATIC_ROOT = Path(BASE_DIR, 'static')
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
